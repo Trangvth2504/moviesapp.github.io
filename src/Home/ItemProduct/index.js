e749e29499e3411e3f53fcb0f99f5dde37781b1d
Trangvth <trangvth@smartosc.com>
2019-10-17 17:50:48 +0700

    Update content

--- src/Home/ItemProduct/index.js
+++ src/Home/ItemProduct/index.js
@@ -5,21 +5,22 @@ class ItemProduct extends Component {
         super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
         this.state = { //state is by default an object
            items: [
-              { id: 1, 
-                image: '/assets/images/11.jpg', 
-                name: Force 2, 
-                desc: 'Presenting the official trailer of Force 2 starring John Abraham, Sonakshi Sinha and Tahir Raj Bhasin. A film by Abhinay Deo JA entertainment Pvt. Ltd....', 
-                date: 'Sep 29 2016 ', 
-                tags: 'Drama'
-            },
-              { 
-                id: 2, 
-                image: '/assets/images/m1.jpg', 
-                name: Swiss Army Man, 
-                desc: 'Presenting the official trailer of Force 2 starring John Abraham, Sonakshi Sinha and Tahir Raj Bhasin. A film by Abhinay Deo JA entertainment Pvt. Ltd....', 
-                date: 'Sep 29 2016 ', 
-                tags: 'Drama'}
-              }
+                { 
+                    id: 1, 
+                    image: '/assets/images/11.jpg', 
+                    name: Force 2, 
+                    desc: 'Presenting the official trailer of Force 2 starring John Abraham, Sonakshi Sinha and Tahir Raj Bhasin. A film by Abhinay Deo JA entertainment Pvt. Ltd....', 
+                    date: 'Sep 29 2016 ', 
+                    tags: 'Drama'
+                },
+                { 
+                    id: 2, 
+                    image: '/assets/images/m1.jpg', 
+                    name: Swiss Army Man, 
+                    desc: 'Presenting the official trailer of Force 2 starring John Abraham, Sonakshi Sinha and Tahir Raj Bhasin. A film by Abhinay Deo JA entertainment Pvt. Ltd....', 
+                    date: 'Sep 29 2016 ', 
+                    tags: 'Drama'
+                }
            ]
         }
     }
